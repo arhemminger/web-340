84872b90328bd357126b662a7705248c5667cd46
Andrew Hemminger <arhemminger@gmail.com>
2019-03-03 13:23:18 -0600

    Adding initial files to routes folder

---
+++ week-2/routes/hemminger-assignment-2.3.js
@@ -0,0 +1,14 @@
+/*
+============================================
+; Title:  Hello World with Express
+; Author: Professor Krasso
+; Date:  2 March 2019
+; Modified by: Andrew Hemminger
+; Description: Exercise 2.2 – Hello World with Express
+;===========================================
+*/
+var header = require('./hemminger-header.js');
+console.log(header.display('Andrew', 'Hemminger', 'Exercise 2.2'));
+console.log('\n');
+
+// start program
